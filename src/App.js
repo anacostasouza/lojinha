9a834829abcf164b611d319cd8d5dae8eee06a54
Ana Souza <anamariacostasouza93@gmail.com>
2025-03-27 11:14:46 -0300

    Removes unused components from App.js
    Eliminates Frase and List components to streamline the codebase.
    
    Improves maintainability by reducing unnecessary dependencies.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Frase from './components/Frase';
-import List from './components/List';
 import Item from './components/Item';
 
 function App() {
   return (
     <div className="App">
-      <Frase texto="Olá mundo!" />
-      <List />
       <Item item={{ name: 'Fiat', description: 'Marca de Carro conhecida ' }} />  
     </div>
   );
